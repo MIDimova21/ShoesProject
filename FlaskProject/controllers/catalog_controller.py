5273a5174a0abc51906bbbb5954fc738994358e9
MIDimova21 <MIDimova21@codingburgas.bg>
2025-09-28 18:12:50 +0300

    Create catalog for the shoes

--- FlaskProject/controllers/catalog_controller.py
+++ FlaskProject/controllers/catalog_controller.py
@@ -1,10 +1,21 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 from FlaskProject.services import catalog_service
 
 catalog_bp = Blueprint('catalog', __name__)
 
-@catalog_bp.route('/catalog')
+@catalog_bp.route("/catalog")
 def show_catalog():
-    products = catalog_service.get_all_products()
-    return render_template('catalog.html', products=products)
+    query = request.args.get("query")
+    max_price = request.args.get("max_price", type=int)
+    in_stock = request.args.get("in_stock") == "on"
+
+    products = catalog_service.filter_products(query, max_price, in_stock)
+
+    return render_template(
+        "catalog.html",
+        products=products,
+        search_query=query,
+        max_price=max_price,
+        in_stock=in_stock
+    )
--- FlaskProject/services/catalog_service.py
+++ FlaskProject/services/catalog_service.py
@@ -6,3 +6,29 @@ products = [
 
 def get_all_products():
     return products
+
+def search_products(query):
+    query = query.lower()
+    return [
+        product for product in products
+        if query in product["name"].lower() or query in product["color"].lower()
+    ]
+
+def filter_products(query=None, max_price=None, in_stock=False):
+    result = products
+
+    # Search filter
+    if query:
+        q = query.lower()
+        result = [p for p in result if q in p["name"].lower() or q in p["color"].lower()]
+
+    # Price filter
+    if max_price:
+        result = [p for p in result if p["price"] <= max_price]
+
+    # Stock filter
+    if in_stock:
+        result = [p for p in result if p["stock"] > 0]
+
+    return result
+
